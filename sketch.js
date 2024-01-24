d57c59eff82104d7352e0572a7572f1394a258f2
Philipp Lehmann <philipp@redefine.studio>
2024-01-24 22:08:13 +0100

    SVG writing

--- sketch.js
+++ sketch.js
@@ -4,6 +4,7 @@ let boxId = 0;
 let prevBoxes = [];
 let currentFaceImg = null;
 let currentEdgeImg = null;
+let currentSVGData = null;
 
 
 currentFaceImg = null;
@@ -12,7 +13,10 @@ let trained = false;
 const imgSize = 64;
 
 function setup() {
-    createCanvas(900, 520);
+    let container = select('#p5container');
+    let canvas = createCanvas(900, 520);
+    canvas.parent(container);
+
     initVideo();
     initFaceDetector();
 }
@@ -104,6 +108,8 @@ function getBoxes(detections) {
             box.label = "ID" + boxId; // Assign a new ID
             currentFaceImg = getCroppedImage(box);
             currentEdgeImg = applyEdgeDetection(currentFaceImg);
+            currentSVGData = convertToSVGPath(currentEdgeImg);
+            appendSVGToContainer(currentSVGData, 'svgOutput', currentEdgeImg.width, currentEdgeImg. height);
         }
 
         newBoxes.push(box);
@@ -161,3 +167,77 @@ function applyEdgeDetection(img) {
     return edgeImg;
 }
 
+function convertToSVGPath(img) {
+    let pixels = img.pixels;
+    let src = new cv.Mat(img.height, img.width, cv.CV_8UC4);
+    src.data.set(pixels);
+
+    // Convert the image to grayscale
+    let gray = new cv.Mat();
+    cv.cvtColor(src, gray, cv.COLOR_RGBA2GRAY);
+
+    // Apply Canny edge detection
+    let edges = new cv.Mat();
+    cv.Canny(gray, edges, 50, 150, 3, false);
+
+    // Find contours in the edge image
+    let contours = new cv.MatVector();
+    let hierarchy = new cv.Mat();
+    cv.findContours(edges, contours, hierarchy, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE);
+    console.log("Number of Paths Found:", contours.size());
+
+    // Convert all contours to SVG path data
+    let svgPathData = '';
+    for (let j = 0; j < contours.size(); ++j) {
+        let contour = contours.get(j);
+
+        if (j < 5) { // Log points only for the first 5 contours
+            console.log(`Contour ${j}, Number of Points: ${contour.data32S.length / 2}`);
+            for (let i = 0; i < contour.data32S.length; i += 2) {
+                let x = contour.data32S[i];
+                let y = contour.data32S[i + 1];
+                svgPathData += (i === 0 ? 'M' : 'L') + x + ' ' + y + ' ';
+                console.log(`Contour ${j} Point ${i/2}: x = ${x}, y = ${y}`);
+            }
+        } else {
+            for (let i = 0; i < contour.data32S.length; i += 2) {
+                let x = contour.data32S[i];
+                let y = contour.data32S[i + 1];
+                svgPathData += (i === 0 ? 'M' : 'L') + x + ' ' + y + ' ';
+            }
+        }
+        
+        svgPathData += 'Z ';
+    }
+
+    // Release Mats to free memory
+    src.delete();
+    gray.delete();
+    edges.delete();
+    contours.delete();
+    hierarchy.delete();
+
+    return svgPathData;
+}
+
+function appendSVGToContainer(pathData, containerId, w, h) {
+    // Create an SVG element
+    let svgElement = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+
+    // Set SVG dimensions to match the edgeImg
+    svgElement.setAttribute("width", w);
+    svgElement.setAttribute("height", h);
+
+    // Create a path element and set the path data
+    let pathElement = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    pathElement.setAttribute("d", pathData);
+    pathElement.setAttribute("fill", "none");
+    pathElement.setAttribute("stroke", "black"); // You can customize stroke color
+
+    // Append the path to the SVG element
+    svgElement.appendChild(pathElement);
+
+    // Append the SVG element to the specified container
+    let container = document.getElementById(containerId);
+    container.appendChild(svgElement);
+}
